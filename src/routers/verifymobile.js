b0fd19b95ab2b4a8c643aa3caa003017b3fffca7
Dhroov Gupta <dhroovgupta7@gmail.com>
2018-09-28 17:57:43 +0530

    verify mobile task completed

--- src/routers/verifymobile.js
+++ src/routers/verifymobile.js
@@ -6,21 +6,86 @@
 const route = require('express').Router()
 const models = require('../db/models').models
 const cel = require('connect-ensure-login')
+const {findUserById} = require('../controllers/user')
+const Raven = require('raven')
 
-route.get('/', cel.ensureLoggedIn('/login'), (req, res) => {
+route.get('/', cel.ensureLoggedIn('/login'), async (req, res) => {
 
-    const user = models.findOne({
-        where: {
-            id: req.user.id
+    try {
+
+        const user = await findUserById(req.user.id)
+
+        if (!user) {
+            req.flash('Invalid user, Please login.')
+            res.redirect('/login')
         }
-    })
 
-    if (!user) {
-        req.flash('Invalid user, Please login.')
-        res.redirect('/login')
+        const key = Math.floor(Math.random() * 1000000) //creates a 6 digit random number.
+
+        await models.Verifymobile.upsert({
+            mobile_number: user.dataValues.mobile_number,
+            key: key,
+            userId: req.user.id,
+            include: [models.User]
+        })
+
+        return res.render('verifymobile', {user})
+
+    } catch (err) {
+        Raven.captureException(err)
+        req.flash('error', 'Could not verify mobile number.')
+        res.redirect('/users/me')
+    }
+})
+
+route.post('/verify', cel.ensureLoggedIn('/login'), async (req, res) => {
+
+    try {
+
+        if (req.body.otp.trim() === '') {
+            req.flash('error', 'OTP cannot be empty')
+            return res.redirect('/verifymobile')
+        }
+
+        const user = await models.User.findOne({
+            where: {
+                verifiedmobile: req.body.mobile_number
+            }
+        })
+
+        if (user) {
+            // Mobile Number already verified, take person to profile page
+            req.flash('error', 'Mobile number already verified with codingblocks account ID:' + user.get('id'))
+            return res.redirect('/users/me')
+        }
+
+        const key = await models.Verifymobile.findOne({
+            where: {
+                userId: req.user.id
+            }
+        })
+
+        if (key.dataValues.key === req.body.otp) {
+            await models.User.update({verifiedmobile: req.body.mobile_number}, {
+                where: {
+                    id: req.user.id
+                }
+            })
+
+        }else{
+            req.flash('error', 'You have entered an incorrect OTP.')
+            return res.redirect('/verifymobile')
+        }
+
+        req.flash('info', 'Your mobile number is verified. Thank you.')
+        return res.redirect('/users/me')
+
+    } catch (err) {
+        Raven.captureException(err)
+        req.flash('error', 'Could not verify mobile number.')
+        res.redirect('/users/me')
     }
 
-    res.render('verifymobile', {user: user.dataValues})
 })
 
 module.exports = route
