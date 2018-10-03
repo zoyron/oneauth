9418f1e2a92f7c41e095bc3786538e92be96e47a
Arnav Gupta <dev@championswimmer.in>
2018-10-03 21:42:00 +0530

    back to edit page

--- src/routers/api/address.js
+++ src/routers/api/address.js
@@ -67,7 +67,7 @@ router.post('/:id', cel.ensureLoggedIn('/login'), async function (req, res) {
 
     if(!req.body.label){
         req.flash('error', 'Please provide the label of the address.')
-        return res.redirect('/address')
+        return res.redirect('/address/' + req.params.id + '/edit)
     }
 
 
