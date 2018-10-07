1b1ba217fb1ad196bb43fbde38a44a96e396641e
Arnav Gupta <arnav@codingblocks.com>
2018-10-07 16:05:09 +0530

    from edit, return back to where user came from
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- src/middlewares/profilevalidation.js
+++ src/middlewares/profilevalidation.js
@@ -1,14 +1,25 @@
-
 function redirectToEditProfile(req, res, next) {
+    let returnTo = '/users/me'
 
-  if (req.path === "/users/me/edit") {
-      return next();
-  }
-  if (req.user && (!req.user.email || !req.user.mobile_number)) {
-    return res.redirect("/users/me/edit");
-  }
-  return next();
+    if (req.query && req.query.returnTo) {
+        returnTo = req.query.returnTo
+    }
+    if (req.body && req.body.returnTo) {
+        returnTo = req.query.returnTo
+    }
+    if (req.session && req.session.returnTo) {
+        returnTo = req.query.returnTo
+    }
+
+    if (req.path === "/users/me/edit") {
+        return next()
+    }
+    if (req.user && (!req.user.email || !req.user.mobile_number)) {
+        return res.redirect(`/users/me/edit/?returnTo=${returnTo}`)
+    }
+    return next()
 }
+
 module.exports = {
-  redirectToEditProfile
-};
+    redirectToEditProfile
+}
