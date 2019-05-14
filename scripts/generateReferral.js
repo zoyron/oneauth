9abb4b2d5c97a7eede69b4f5b0df461ea369785d
tdevm <cr14b17@gmail.com>
2019-05-14 09:42:08 +0530

    referral generator script

---
+++ scripts/generateReferral.js
@@ -0,0 +1,28 @@
+/**
+ * Created by tdevm on 14/5/19.
+ */
+
+const {
+    db, models: {
+        User
+    }
+} = require('.././src/db/models');
+const {generateReferralCode} = require('../src/utils/referral');
+
+User.findAll().then((users) => {
+    let usersPromise  = [];
+    users.forEach((user) => {
+        usersPromise.push(user.update({
+            referralCode: generateReferralCode(user.get().username)
+        }))
+    });
+    return usersPromise
+}).then((usersPromise) => {
+  return Promise.all(usersPromise)
+}).then((updated) => {
+    console.log('Referral code generation complete')
+}).catch((err) => {
+    console.error(err);
+}).finally(() => {
+    process.exit(0);
+});
