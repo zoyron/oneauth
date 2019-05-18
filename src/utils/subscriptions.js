b0ce5f9b0b8f5ea546cd489f1296612dcd5bb368
Manit Malhotra <manitmalhotra3@gmail.com>
2019-05-18 15:30:07 +0530

    check for null too

--- src/utils/subscriptions.js
+++ src/utils/subscriptions.js
@@ -17,14 +17,18 @@ function sendEvent(subscriptions, model, type, id, userId) {
   return Promise.all(subscriptions.map(async subscription => {
     const client = await Client.findById(subscription.clientId)
     const webhookURL = client.webhookURL
-    if (webhookURL.match(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g))
-    return request.post(webhookURL, {
-      type,
-      model,
-      success: 'true',
-      id,
-      userId
-    })
+    if (
+      webhookURL &&
+      webhookURL.match(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g)
+    ) {
+      return request.post(webhookURL, {
+        type,
+        model,
+        success: 'true',
+        id,
+        userId
+      })
+    }
   }))
 }
 
