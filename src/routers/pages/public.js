bed5e1ed9d0aa83c39bd023f249e98e3e4b0550a
tdevm <cr14b17@gmail.com>
2019-05-29 13:48:57 +0530

    remove for

--- src/routers/pages/public.js
+++ src/routers/pages/public.js
@@ -37,10 +37,9 @@ router.get('/signup', cel.ensureNotLoggedIn('/'), async function (req, res, next
 
         const verifiedRefCode = await findUserByParams({referralCode: req.query.refcode })
 
-        let gradYears = []
-        for(let i=2025;i >= 2000; i--){
-            gradYears.push(i)
-        }
+        const gradYears = [2025, 2024, 2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014,
+            2013, 2012, 2011, 2010, 2009, 2008, 2007, 2006, 2005, 2004, 2003, 2002, 2001, 2000]
+
         // use the previous form only once
         delete req.session.prevForm
 
