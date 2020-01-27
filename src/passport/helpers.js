cccb55dba0cbfa9b010b8b177561b58061192f62
Jatin Katyal <jatin.katyal13@gmail.com>
2020-01-27 18:45:38 +0530

    Revert "Revert "send OTPs for non verified numbers""
    This reverts commit d1879d8d0d24e2fa106a3a57f3e7ba016bcceaa7.

--- src/passport/helpers.js
+++ src/passport/helpers.js
@@ -1,13 +1,14 @@
 const Raven = require('raven')
 const passutils = require("../utils/password")
 const models = require('../db/models').models
+const UserController = require('../controllers/user')
 
 
 const isValidOtpForUser = async (user, otp) => {
     try {
         const lastLoginOTP = await models.UserMobileOTP.findOne({
             where: {
-                mobile_number: user.get().verifiedmobile,
+                userId: user.get().id,
                 used_at: null
             },
             order: [['createdAt', 'DESC']]
@@ -17,7 +18,7 @@ const isValidOtpForUser = async (user, otp) => {
             return false
         }
 
-        if (lastLoginOTP.get('login_otp') === otp && (new Date(lastLoginOTP.dataValues.createdAt).getTime() > (new Date().getTime() - 10 * 60 * 1000)) ) {
+        if (lastLoginOTP.get('login_otp') === otp && (new Date(lastLoginOTP.dataValues.createdAt).getTime() > (new Date().getTime() - 10 * 60 * 1000))) {
             await lastLoginOTP.update({
                 used_at: new Date()
             })
@@ -25,7 +26,7 @@ const isValidOtpForUser = async (user, otp) => {
         }
 
         return false
-    } catch(err)  {
+    } catch (err) {
         Raven.captureException(err)
         return false
     }
@@ -35,6 +36,16 @@ const isValidPasswordForUser = async (user, password) => {
     return await passutils.compare2hash(password, user.password)
 }
 
-module.exports = {
-    isValidOtpForUser, isValidPasswordForUser
+const makeTempOTPUserPermanent = async (userMobile) => {
+    const user = await models.User.findById(userMobile.id)
+    if (!user) {
+        throw new Error("Temp user not found")
+    }
+    return user.restore()
 }
+
+
+module.exports = {
+    isValidOtpForUser, isValidPasswordForUser, makeTempOTPUserPermanent
+}
+
