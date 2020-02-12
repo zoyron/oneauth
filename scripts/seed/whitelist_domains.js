89826e9d50df454c587b77737f7339961b1bf8d6
Jatin Katyal <jatin.katyal13@gmail.com>
2020-02-12 15:42:11 +0530

    seed update

--- scripts/seed/whitelist_domains.js
+++ scripts/seed/whitelist_domains.js
@@ -2,8 +2,8 @@ const { db } = require('../../src/db/models');
 
 (async () => {
   db.query(`
-    INSERT INTO whitelist_domains (
-      SELECT DISTINCT LOWER(SPLIT_PART(verifiedemail, '@', 2)) FROM users WHERE verifiedemail IS NOT NULL
+    INSERT INTO whitelist_domains (domain, "createdAt", "updatedAt") (
+      SELECT DISTINCT LOWER(SPLIT_PART(verifiedemail, '@', 2)), NOW(), NOW() FROM users WHERE verifiedemail IS NOT NULL
     )
   `)
 })()
