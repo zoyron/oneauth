184d821627618a8016774532be9d53a57c525151
Jatin Katyal <jatin.katyal13@gmail.com>
2020-02-13 17:03:12 +0530

    filter bad bad domains

--- scripts/seed/whitelist_domains.js
+++ scripts/seed/whitelist_domains.js
@@ -1,9 +1,18 @@
 const { db } = require('../../src/db/models');
 
 (async () => {
-  db.query(`
+  await db.query(`
     INSERT INTO whitelist_domains (domain, "createdAt", "updatedAt") (
       SELECT DISTINCT LOWER(SPLIT_PART(verifiedemail, '@', 2)), NOW(), NOW() FROM users WHERE verifiedemail IS NOT NULL
     )
+  `)
+  return db.query(`
+    DELETE FROM whitelist_domains WHERE domain IN
+      ( 'guerrillamail.com',
+        'mailinator.com',
+        'qq.com',
+        'trash-mail.com',
+        'yopmail.com'
+      )  
   `)
 })()
